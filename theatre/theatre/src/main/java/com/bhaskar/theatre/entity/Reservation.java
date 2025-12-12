3c9a206b385aca34f50dd740710b37edd011671d
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-12 16:20:58 +0530

    Reservation

--- theatre/theatre/src/main/java/com/bhaskar/theatre/entity/Reservation.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/entity/Reservation.java
@@ -1,5 +1,41 @@
 package com.bhaskar.theatre.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Show show;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Seat> seatsReserved;
+
+    private double amountPaid;
+
+    @Enumerated(value = EnumType.STRING)
+    private ReservationStatus reservationStatus;
+
+    private LocalDate createdAt;
+
 
 }
