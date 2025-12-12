64016f7b3e045523205d8d9f290c98a07bab6ce9
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-12 16:29:51 +0530

    Seat

--- theatre/theatre/src/main/java/com/bhaskar/theatre/entity/Seat.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/entity/Seat.java
@@ -1,9 +1,27 @@
 package com.bhaskar.theatre.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Seat {
 
-    public enum Role {
-        ADMIN,
-        USER
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Enumerated(value = EnumType.STRING)
+    private SeatStatus status;
+
+    private double price;
+    private int number;
+    private String area;
 }
