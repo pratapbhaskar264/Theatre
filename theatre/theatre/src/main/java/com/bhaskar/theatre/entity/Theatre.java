2ca1ded1a9257cc44e0deabd4807349fa657da2a
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-12 16:49:24 +0530

    theatre

--- theatre/theatre/src/main/java/com/bhaskar/theatre/entity/Theatre.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/entity/Theatre.java
@@ -1,4 +1,25 @@
 package com.bhaskar.theatre.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class Theatre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    long id;
+    String name;
+    String location;
 }
