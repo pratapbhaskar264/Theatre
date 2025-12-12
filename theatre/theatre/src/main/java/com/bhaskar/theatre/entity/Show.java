bcbb20ad84cb263dda2180c56062eb1e8acc5cd9
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-12 16:47:09 +0530

    Seat

--- theatre/theatre/src/main/java/com/bhaskar/theatre/entity/Show.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/entity/Show.java
@@ -1,4 +1,26 @@
 package com.bhaskar.theatre.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Show {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @ManyToOne(targetEntity = Movie.class)
+            @JoinColumn(referencedColumnName = "id" , nullable = false)
+    Movie movie;
+
+    @ManyToOne()
+    Theatre theatre;
+    LocalDateTime starTime;
+    LocalDateTime endTime;
+
+    @OneToMany(fetch = FetchType.LAZY , cascade = CascadeType.REMOVE)
+    List<Seat> seats;
 }
