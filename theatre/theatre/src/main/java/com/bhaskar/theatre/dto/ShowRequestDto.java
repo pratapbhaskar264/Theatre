ae1d824df27a3210b3cf71928c047c75d8fcd9bc
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-13 18:15:46 +0530

    Show-dto

--- theatre/theatre/src/main/java/com/bhaskar/theatre/dto/ShowRequestDto.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/dto/ShowRequestDto.java
@@ -1,4 +1,17 @@
 package com.bhaskar.theatre.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class ShowRequestDto {
+
+    private long movieId;
+    private long theatreId;
+    private String startTime;
+    private String endTime;
+    private List<SeatStructure> seats;
 }
