c443df5d38ae2de07a3bf41de2894719d498a79d
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-14 16:29:10 +0530

    exception-custom

--- theatre/theatre/src/main/java/com/bhaskar/theatre/exception/CustomException.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/exception/CustomException.java
@@ -1,4 +1,13 @@
 package com.bhaskar.theatre.exception;
 
-public class CustomException {
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException{
+    String message;
+    HttpStatus httpStatus;
+
+    public CustomException(String message, HttpStatus httpStatus ) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
 }
