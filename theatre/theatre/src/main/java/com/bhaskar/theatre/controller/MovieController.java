5dfd9c9a54ad728e52fe00af9497650414d31516
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-14 17:09:56 +0530

    movie module setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/controller/MovieController.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/controller/MovieController.java
@@ -1,9 +1,19 @@
 package com.bhaskar.theatre.controller;
 
 
+
+import com.bhaskar.theatre.dto.PagedApiResponseDto;
+import com.bhaskar.theatre.entity.Movie;
+import com.bhaskar.theatre.service.MovieService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN')")
 @RestController
@@ -13,5 +23,24 @@ public class MovieController {
     private final MovieService movieService;
 
 
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<PagedApiResponseDto> getAllMovies( @RequestParam(defaultValue = "0") int page,
+                                                             @RequestParam(defaultValue = "10") int pageSize) {
+        Page<Movie> moviePage = movieService.getAllMovies(page, pageSize);
+        List<Movie> movies = moviePage.getContent();
+        return ResponseEntity.ok(
+                PagedApiResponseDto.builder()
+                        .totalPages(moviePage.getTotalPages())
+                        .totalElements(moviePage.getTotalElements())
+                        .currentPageData(movies)
+                        .build()
+        );
+    }
+
+    @GetMapping("/movie/{movieId}")
 }
 
--- theatre/theatre/src/main/java/com/bhaskar/theatre/service/MovieService.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/MovieService.java
@@ -1,4 +1,10 @@
 package com.bhaskar.theatre.service;
 
+import com.bhaskar.theatre.entity.Movie;
+import org.springframework.data.domain.Page;
+
 public class MovieService {
+    public Page<Movie> getAllMovies(int page, int pageSize) {
+        return null;
+    }
 }
