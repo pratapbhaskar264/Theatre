3c4842e310eddc8496b9437c6e222b201b6c16c7
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-15 14:08:35 +0530

    reservation module setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/controller/ReservationController.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/controller/ReservationController.java
@@ -1,4 +1,10 @@
 package com.bhaskar.theatre.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reservations")
 public class ReservationController {
 }
