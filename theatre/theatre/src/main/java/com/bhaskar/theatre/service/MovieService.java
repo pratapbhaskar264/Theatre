f900acc0a36574948ef54b4368819f349ac166f1
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-15 11:14:12 +0530

    movie module setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/service/MovieService.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/MovieService.java
@@ -2,11 +2,26 @@ package com.bhaskar.theatre.service;
 
 import com.bhaskar.theatre.dto.MovieRequestDto;
 import com.bhaskar.theatre.entity.Movie;
+import com.bhaskar.theatre.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieService(MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+    }
+
+
     public Page<Movie> getAllMovies(int page, int pageSize) {
-        return null;
+        return movieRepository.findAll(PageRequest.of(page,pageSize)) ;
     }
 
     public Movie getMovieById(long movieId) {
