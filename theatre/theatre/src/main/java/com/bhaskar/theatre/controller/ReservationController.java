3ff7f750732b79a67f20cb002bbe9635ed6d8ab0
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-15 14:22:48 +0530

    reservation module setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/controller/ReservationController.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/controller/ReservationController.java
@@ -1,10 +1,33 @@
 package com.bhaskar.theatre.controller;
 
 
+import com.bhaskar.theatre.dto.PagedApiResponseDto;
+import com.bhaskar.theatre.service.ReservationService;
+import jakarta.persistence.GeneratedValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/reservations")
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping("/user/all")
+    public ResponseEntity<PagedApiResponseDto> getAllReservationsForCurrentUser(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        return
+    }
+
 }
---
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/repository/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.bhaskar.theatre.repository;
+
+import com.bhaskar.theatre.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
+
