a0ddb56e39c8c3b1c6dbc4673acb2f8ea5feee22
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-16 14:15:58 +0530

    theatre-module-setup

---
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/TheatreService.java
@@ -0,0 +1,13 @@
+package com.bhaskar.theatre.service;
+
+import com.bhaskar.theatre.entity.Theatre;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TheatreService {
+    public Page<Theatre> getAllTheatres(int page, int size) {
+
+
+    }
+}
