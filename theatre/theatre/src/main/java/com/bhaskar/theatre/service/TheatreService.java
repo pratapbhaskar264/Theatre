45cda41e97f696679733f45c9b863c796854f693
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-16 14:19:44 +0530

    theatre-module-setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/service/TheatreService.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/TheatreService.java
@@ -1,13 +1,26 @@
 package com.bhaskar.theatre.service;
 
 import com.bhaskar.theatre.entity.Theatre;
+import com.bhaskar.theatre.repository.TheatreRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TheatreService {
-    public Page<Theatre> getAllTheatres(int page, int size) {
 
 
+        private final TheatreRespository theatreRepository;
+
+    public TheatreService(TheatreRespository theatreRepository) {
+        this.theatreRepository = theatreRepository;
     }
+
+
+    public Page<Theatre> getAllTheatres(int page, int size) {
+            return theatreRepository.findAll(PageRequest.of(page, size));
+    }
+
+
 }
