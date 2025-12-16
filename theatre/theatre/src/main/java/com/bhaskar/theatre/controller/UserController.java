808312a7465529bceea1a83560a2e8698711f054
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-16 17:16:28 +0530

    user-module-setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/controller/UserController.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/controller/UserController.java
@@ -1,4 +1,40 @@
 package com.bhaskar.theatre.controller;
 
+import com.bhaskar.theatre.dto.UserResponseDto;
+import com.bhaskar.theatre.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user/me")
+    public ResponseEntity<UserResponseDto> currentUser() {
+        String currentUsername = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userRepository.findByUsername(currentUsername)
+                .map(user -> ResponseEntity.ok(UserResponseDto.builder()
+                        .email(user.getEmail())
+                        .firstName(user.getFirstName())
+                        .lastName(user.getLastName())
+                        .role(user.getRole())
+                        .username(user.getUsername())
+                        .id(user.getId())
+                        .build())
+                )
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND));
+    }
+
+
+
 }
---
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/exception/UsernameNotFoundException.java
@@ -0,0 +1,9 @@
+package com.bhaskar.theatre.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class UsernameNotFoundException extends CustomException{
+    public UsernameNotFoundException(String message, HttpStatus httpStatus) {
+        super(message, httpStatus);
+    }
+}
