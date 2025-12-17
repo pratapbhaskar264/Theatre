d9fb52c2d95ba307acde655e5569dfc46bb1e0f4
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-17 17:41:43 +0530

    seat-module-setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/service/SeatService.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/SeatService.java
@@ -1,8 +1,13 @@
 package com.bhaskar.theatre.service;
 
+import com.bhaskar.theatre.entity.Seat;
+import com.bhaskar.theatre.enums.SeatStatus;
 import com.bhaskar.theatre.repository.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Service
 public class SeatService {
@@ -13,5 +18,16 @@ public class SeatService {
         this.seatRepository = seatRepository;
     }
 
-
+    public List<Seat> createSeatsWithGivenPrice(int seats, double price, String area) {
+        return IntStream.range(1, seats + 1)
+                .mapToObj(seatCount -> Seat.builder()
+                        .price(price)
+                        .number(seatCount)
+                        .area(area)
+                        .status(SeatStatus.UNBOOKED)
+                        .build()
+                )
+                .map(seatRepository::save)
+                .toList();
+    }
 }
