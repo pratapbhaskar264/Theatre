04e68878823551ce4878d17d051db63dc28ef892
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-17 17:36:32 +0530

    seat-module-setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/service/SeatService.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/SeatService.java
@@ -1,4 +1,17 @@
 package com.bhaskar.theatre.service;
 
+import com.bhaskar.theatre.repository.SeatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SeatService {
+    private final SeatRepository seatRepository;
+
+    @Autowired
+    public SeatService(SeatRepository seatRepository) {
+        this.seatRepository = seatRepository;
+    }
+
+
 }
