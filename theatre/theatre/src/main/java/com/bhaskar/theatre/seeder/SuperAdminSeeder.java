d4a28688669c5d7890e6f0c84795a2fb3a43e415
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-20 16:52:29 +0530

    Seeder-setup

--- theatre/theatre/src/main/java/com/bhaskar/theatre/seeder/SuperAdminSeeder.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/seeder/SuperAdminSeeder.java
@@ -1,4 +1,16 @@
 package com.bhaskar.theatre.seeder;
 
+import com.bhaskar.theatre.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SuperAdminSeeder {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public SuperAdminSeeder(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 }
