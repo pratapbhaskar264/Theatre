ad672028ddacee7d82998b90d4c3fbb4b18994e8
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-21 16:29:46 +0530

    security

--- theatre/theatre/src/main/java/com/bhaskar/theatre/controller/ControllerAdvice.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/controller/ControllerAdvice.java
@@ -1,4 +1,21 @@
 package com.bhaskar.theatre.controller;
 
+
+import com.bhaskar.theatre.exception.CustomException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
 public class ControllerAdvice {
+
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Map<String, String>> handleCustomException(CustomException customException){
+        return ResponseEntity
+                .status(customException.getHttpStatus())
+                .body(Map.of("message", customException.getMessage()));
+    }
 }
