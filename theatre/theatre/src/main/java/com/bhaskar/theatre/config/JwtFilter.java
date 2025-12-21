1ac7fb44ca34919ce78bd631ef9e5b8515352351
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-21 16:26:48 +0530

    security

---
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/config/JwtFilter.java
@@ -0,0 +1,58 @@
+package com.bhaskar.theatre.config;
+
+//package com.bhaskar.theatre.config;
+
+import com.bhaskar.theatre.repository.UserRepository;
+import com.bhaskar.theatre.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public JwtFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            String token = authHeader.substring(7);
+            String username = jwtService.extractUsername(token);
+            if(username == null){
+                filterChain.doFilter(request, response);
+                return;
+            }
+            userRepository.findByUsername(username)
+                    .ifPresent(user -> {
+                        UsernamePasswordAuthenticationToken authToken =
+                                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    });
+
+        }
+        filterChain.doFilter(request, response);
+    }
+}
