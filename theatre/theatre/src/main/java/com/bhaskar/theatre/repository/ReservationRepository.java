f6419df95918a26328cb471552d0cb027af36bba
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-22 13:41:46 +0530

    general-repoLayerFix

--- theatre/theatre/src/main/java/com/bhaskar/theatre/repository/ReservationRepository.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/repository/ReservationRepository.java
@@ -1,21 +1,11 @@
 package com.bhaskar.theatre.repository;
 
 import com.bhaskar.theatre.entity.Reservation;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.awt.print.Pageable;
-
-//public interface ReservationRepository
-//        extends JpaRepository<Reservation, Long>,
-//        JpaSpecificationExecutor<Reservation> {
-//}
+import org.springframework.stereotype.Repository;
 
 
-public interface ReservationRepository
-        extends JpaRepository<Reservation, Long> {
-
-    Page<Reservation> findByUserUsername(String username, Pageable pageable);
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
 
