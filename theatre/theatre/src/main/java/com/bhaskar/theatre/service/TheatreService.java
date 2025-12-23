8ec23ad546d6bbbe9a95fd3a1a0eb3b4ae1cfb08
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2025-12-23 12:53:30 +0530

    MainClassControllerRemoval

--- theatre/theatre/src/main/java/com/bhaskar/theatre/service/TheatreService.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/TheatreService.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 
 import static com.bhaskar.theatre.constant.ExceptionMessages.THEATRE_NOT_FOUND;
 
@@ -20,6 +19,7 @@ public class TheatreService {
 
         private final TheatreRespository theatreRepository;
 
+        @Autowired
     public TheatreService(TheatreRespository theatreRepository) {
         this.theatreRepository = theatreRepository;
     }
