9e686de8054e90787f99dae2592951df58d95c0f
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-01-17 18:24:45 +0530

    redis-serviceAdded

---
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/RedisService.java
@@ -0,0 +1,31 @@
+package com.bhaskar.theatre.service;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+public class RedisService {
+
+    private final RedisTemplate<String ,Object> redisTemplate;
+
+
+    public RedisService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void set(String key, Object value, Long timeoutInMinutes) {
+            redisTemplate.opsForValue().set(key, value, timeoutInMinutes, TimeUnit.MINUTES);
+    }
+
+    // generic type for all type of class objects
+    public <T> T get(String key, Class<T> entityClass) {
+            Object value = redisTemplate.opsForValue().get(key);
+            if (value == null) return null;
+            return entityClass.cast(value);
+    }
+
+    //for update and delete
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+}
