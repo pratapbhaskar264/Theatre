0606e12597ed655a5af355ecdc39c200ffa69600
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-01-26 17:08:40 +0530

    kafka-consumer-dto

--- theatre/theatre/src/main/java/com/bhaskar/theatre/service/KafkaConsumerService.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/KafkaConsumerService.java
@@ -10,7 +10,9 @@ public class KafkaConsumerService {
 
     @KafkaListener(topics = "theatre-activity" ,groupId = "theatre-monitoring-group")
     public void consumeBookingActivity(Reservation reservation){
-
+        System.out.println("LOGGING: Received new activity from Kafka!");
+//        System.out.println("User: " + reservation.getUser().getUsername() + " booked seats for Show ID: " + reservation.getShow().getId());
+        // In the future, you could save this to a 'History' table or an Analytics DB
     }
 
 
