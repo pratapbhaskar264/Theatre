2ae72563e264a459f68931d182f58e542b848b1f
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-01-26 17:10:44 +0530

    kafka-consumer-Withdto

--- theatre/theatre/src/main/java/com/bhaskar/theatre/service/KafkaConsumerService.java
+++ theatre/theatre/src/main/java/com/bhaskar/theatre/service/KafkaConsumerService.java
@@ -1,19 +1,19 @@
 package com.bhaskar.theatre.service;
 
-
-import com.bhaskar.theatre.entity.Reservation;
+import com.bhaskar.theatre.dto.BookingEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
 
-    @KafkaListener(topics = "theatre-activity" ,groupId = "theatre-monitoring-group")
-    public void consumeBookingActivity(Reservation reservation){
-        System.out.println("LOGGING: Received new activity from Kafka!");
-//        System.out.println("User: " + reservation.getUser().getUsername() + " booked seats for Show ID: " + reservation.getShow().getId());
-        // In the future, you could save this to a 'History' table or an Analytics DB
+    @KafkaListener(topics = "theatre-activity", groupId = "theatre-monitoring-group")
+    public void consumeBookingActivity(BookingEvent event) {
+        System.out.println("\n--- KAFKA MESSAGE RECEIVED ---");
+        System.out.println("Booking Confirmed for User: " + event.getUsername());
+        System.out.println("Movie: " + event.getMovieName());
+        System.out.println("Seats: " + event.getSeatIds());
+        System.out.println("Total Amount: ₹" + event.getAmount());
+        System.out.println("-------------------------------\n");
     }
-
-
 }
